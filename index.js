abc81ddbd33d55a4406bcd0184c6715858ac4d57
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-12 11:15:34 +0900

    Add browser window

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello Electron.");
+const { app, BrowserWindow } = require('electron');
+const ROOT_PATH = `file://${__dirname}`;
+
+app.on("ready", e => {
+  const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+  mainWindow.loadURL(`${ROOT_PATH}/index.html`);
+});
+
+app.on("window-all-closed", e => {
+  app.quit();
+});
