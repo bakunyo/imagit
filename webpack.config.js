2a3edc5a574f7dbeb26991c83359e63d2d4f2246
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-26 09:03:10 +0900

    Clean app and restart

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './source/index',
+  output: {
+    path: './distribution/',
+    filename: "index.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
