45fd010ab1b68414a2789627dbbeb3b0f9aafaad
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-27 16:19:20 +0900

    Install material-ui

--- source/jsx/root.js
+++ source/jsx/root.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react'
 import Header from './header'
 import Main from './main'
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 export default class Root extends Component {
   render() {
     return (
       <div id='root'>
         <Header />
         <Main />
+        <MuiThemeProvider>
+          <RaisedButton label="Default" />
+        </MuiThemeProvider>
       </div>
     )
   }
