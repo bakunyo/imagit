efd20ffacf25850ecf8a7bded64adafdb5094cba
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-27 17:46:34 +0900

    Add Subheader to CommitList

--- source/jsx/commit_list.js
+++ source/jsx/commit_list.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { List, ListItem } from 'material-ui/List'
+import Subheader from 'material-ui/Subheader'
 
 export default class CommitList extends Component {
   render () {
     return (
       <div id='commit_list'>
         <List>
+          <Subheader>
+            <h2>Commit List</h2>
+          </Subheader>
           <ListItem primaryText='commit name' ></ListItem>
           <ListItem primaryText='commit name' ></ListItem>
           <ListItem primaryText='commit name' ></ListItem>
