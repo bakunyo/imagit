07ed1c6e2b475958b256e43fed0599a8ef5c7d89
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-27 21:09:48 +0900

    Install and use simple-git

--- source/jsx/header.js
+++ source/jsx/header.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react'
-import AppBar from 'material-ui/AppBar';
+import AppBar from 'material-ui/AppBar'
+
+import Git from 'simple-git'
+
+function selectRepository() {
+  let path = '/Users/bakunyo/workspace/imagit'
+  let repo = Git(path)
+  repo.log(function(i, commits) {
+    commits.all.slice(0, 10).map((commit) => {
+      console.log(commit.message);
+    });
+  });
+}
 
 const Header = () => (
   <AppBar
     title='Imagit'
+    onLeftIconButtonTouchTap={selectRepository}
   />
 )
 
