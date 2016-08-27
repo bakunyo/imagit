516a41f15b8b5a3c228c0883907eeacab01b8f23
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-27 20:25:26 +0900

    Use stateless functional components

--- source/jsx/header.js
+++ source/jsx/header.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar';
 
-export default class Header extends Component {
-  render () {
-    return (
-      <AppBar title='Imagit' />
-    )
-  }
-}
+const Header = () => (
+  <AppBar
+    title='Imagit'
+  />
+)
+
+export default Header
