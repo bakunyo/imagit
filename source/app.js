6b2886a68b1610e197872819878db3f8f823fb49
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-27 20:22:10 +0900

    Install react-tap-event-plugin, need for material-ui

--- source/app.js
+++ source/app.js
@@ -4,6 +4,10 @@ import Root from './jsx/root'
 import './sass/style'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 ReactDOM.render(
   <MuiThemeProvider>
     <Root />
