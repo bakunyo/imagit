310c72e8063ca029dab50994b3b3fda188a2376b
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-08-28 11:42:07 +0900

    Add redux elements

--- source/app.js
+++ source/app.js
@@ -1,16 +1,52 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import Root from './jsx/root'
+import App from './components/root'
 import './sass/style'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
+import { createStore } from 'redux'
+import { connect, Provider } from 'react-redux';
+
+const initialState = {
+  value: 1
+}
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 }
+    default:
+      return state
+  }
+}
+
+function increment() {
+  return { type: 'INCREMENT' }
+}
+
+function mapStateToProps(state) {
+  return state
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    handleClick: () => { dispatch(increment()) }
+  }
+}
+
+const Root = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
+
+const store = createStore(reducer)
+
 ReactDOM.render(
-  <MuiThemeProvider>
+  <Provider store={store}>
     <Root />
-  </MuiThemeProvider>,
+  </Provider>,
   document.getElementById('root')
 )
